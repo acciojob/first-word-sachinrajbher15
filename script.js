2f611eb1751d75e579df2545d322c3e768fcd847
SACHIN KUMAR RAJBHER <86919694+sachinrajbher15@users.noreply.github.com>
2024-09-23 19:51:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+	const SpaceIndex = s.indexOf(' ');
+	if(SpaceIndex === -1){
+		return s;
+	}
+	return s.substring(0, SpaceIndex);
 }
 
 // Do not change the code below
